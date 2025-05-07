f7c8d0f40fd187c2c169e34f7689709339336a55
Zan Rocha <andre.rocha2302@gmail.com>
2025-05-07 17:41:35 -0300

    feat: Roteiro02 parte01

---
+++ src/roteiro2/parte1/Programa01.java
@@ -0,0 +1,19 @@
+package roteiro2.parte1;
+
+public class Programa01 {
+
+    public static void main(String[] args) {
+        int idade = 18;
+        char letra = 'a';
+        float numeroFloat = 3.14159f;
+        double numeroDouble = 2.71828;
+        String nome = "Isaac Newton";
+
+        System.out.println("Exibir numero inteiro:" + idade);
+        System.out.println("Exibir caracter:" + letra);
+        System.out.println("Exibir numero flutuante:" + numeroFloat);
+        System.out.println("Exibir numero double:" + numeroDouble);
+        System.out.println("Exibir texto:" + nome);
+    }
+
+}
