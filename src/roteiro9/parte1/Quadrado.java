3fd8fb35da74f6ba787c6352e59de3f1afc7f897
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-18 20:33:31 -0300

    feat: roteiro9 parte1 quadrado class

---
+++ src/roteiro9/parte1/Quadrado.java
@@ -0,0 +1,18 @@
+package roteiro9.parte1;
+
+public class Quadrado {
+
+    private double lado;
+
+    public Quadrado(int lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+}
