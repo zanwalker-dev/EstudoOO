ad07dc92fe5b3f6eabc1547309eb907bc7734833
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-18 15:59:32 -0300

    fix: removed abstract class constructor again

--- src/roteiro8/parte1/Principal.java
+++ src/roteiro8/parte1/Principal.java
@@ -3,22 +3,9 @@ package roteiro8.parte1;
 public class Principal {
     public static void main(String[] args) {
         // Criando objetos dos diferentes tipos de veículos
-        Veiculo v01 = new Veiculo("11111-BA", 2010);
         VeiculoCarga v02 = new VeiculoCarga("22222-BA", 2011, 5000);
         VeiculoPasseio v03 = new VeiculoPasseio("33333-BA", 2012, 5);
         VeiculoPequeno v04 = new VeiculoPequeno("44444-BA", 2013, "moto");
-
-        // Testando modificação e exibição dos dados
-        System.out.println("=== Veículo Genérico ===");
-        System.out.println("Placa: " + v01.getPlaca());
-        System.out.println("Ano: " + v01.getAnoFabricacao());
-
-        // Modificando dados do veículo genérico
-        v01.setPlaca("99999-BA");
-        v01.setAnoFabricacao(2015);
-        System.out.println("\nDados modificados:");
-        System.out.println("Placa: " + v01.getPlaca());
-        System.out.println("Ano: " + v01.getAnoFabricacao());
 
         System.out.println("\n=== Veículo de Carga ===");
         System.out.println("Placa: " + v02.getPlaca());
