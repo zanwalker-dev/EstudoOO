3691654b7859a115decff70930a218252381eff4
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-18 20:40:57 -0300

    feat: quadrado implemented with interface methods

--- src/roteiro9/parte1/Quadrado.java
+++ src/roteiro9/parte1/Quadrado.java
@@ -1,6 +1,6 @@
 package roteiro9.parte1;
 
-public class Quadrado {
+public class Quadrado implements FiguraGeometrica{
 
     private double lado;
 
@@ -14,5 +14,20 @@ public class Quadrado {
 
     public void setLado(double lado) {
         this.lado = lado;
+    }
+
+    @Override
+    public String getNomeFigura() {
+        return "Quadrado";
+    }
+
+    @Override
+    public double getArea() {
+        return this.lado * this.lado;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return this.lado * 4;
     }
 }
