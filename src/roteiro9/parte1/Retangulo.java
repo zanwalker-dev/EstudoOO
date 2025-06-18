20c25ddca03e91a47ea5ccbedc5e0e4f297efdd9
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-18 20:35:20 -0300

    feat: roteiro9 parte1 retangulo class

---
+++ src/roteiro9/parte1/Retangulo.java
@@ -0,0 +1,28 @@
+package roteiro9.parte1;
+
+public class Retangulo {
+
+    private double altura;
+    private double largura;
+
+    public Retangulo(double altura, double largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+}
